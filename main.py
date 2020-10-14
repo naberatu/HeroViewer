c0b86d10c904401a8feb22cad9ba9b860810550c
naberatu <naberatu@gmail.com>
2020-10-14 14:19:12 -0700

    Added button, adjusted colors, established frames.

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 
-from tkinter import *
-from tkinter.ttk import *
+import tkinter as tk
 
-window = Tk()
-window.geometry('600x300')  # default window size
+# Creating Window
+# ========================================================
 
-window.config
+window = tk.Tk()
+window.title("My First GUI")    # Window title
+window.geometry("600x300")      # default window size
+window.configure(bg="#038387")   # background color
+window.iconbitmap('C:\\Users\\elite\\Pictures\\Icons\\cog.ico')
 
-frame = Frame(window)
-frame.pack()
+# Creating Frame(s)
+# ========================================================
 
-window.title("My First GUI")
-# label = tkinter.Label(window, text="Henlo").pack()
-bt = Button(window, text="Exit", command=quit)
-bt.pack(side=TOP)   # centers towards top
+window.columnconfigure(0, weight=2, minsize=150)
+window.columnconfigure(1, weight=2, minsize=150)
+window.rowconfigure(0, weight=1, minsize=50)
+
+frame = tk.Frame(
+            master=window,
+            relief=tk.RAISED,
+            borderwidth=1
+        )
+
+frame.grid(row=0, column=1, padx=5, pady=5)
+bt = tk.Button(
+                master=frame,
+                text="Exit",
+                font=('Comic Sans MS', 12),
+                bg="black",
+                fg="white",
+                width=5,
+                height=1,
+                command=quit
+            )
+bt.pack()
+
 
 window.mainloop()
