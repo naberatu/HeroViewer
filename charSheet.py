170478d79162a0e99f5f09f07f19a63739a8c6f2
naberatu <naberatu@gmail.com>
2020-10-21 20:23:30 -0700

    initial commit

---
+++ charSheet.py
@@ -0,0 +1,14 @@
+
+class CharSheet:
+    def __init__(self):
+        name = None
+        role = None
+        level = 0
+        race = None
+        background = None
+        alignment = None
+        stat_block = None
+        ds_success = 0
+        ds_fail = 0
+
+
