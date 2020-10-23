e7178a9ed4f4df770743f8e18202cbd8bedb986c
naberatu <naberatu@gmail.com>
2020-10-22 21:59:05 -0700

    got double-click to work.

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import tkinter as tk
 from charSheet import CharSheet
 
 
-# Creating example item
-# ========================================================
-hero = CharSheet()
-hero.add_feat("Warrior", "Once per short rest, you can take a bonus action.")
-hero.add_feat("Tamer", "Grants +2 Nature")
-hero.add_feat("Alert", "+ Gain +5 Initiative\n"
-                       "+ Can't be surprised while conscious"
-                       "\n+ No stealth advantage for attackers.")
-
 class MainMenu:
     def __init__(self, master):
         self.master = master
+
+        # Example Hero
+        # ========================================================
+        self.hero = CharSheet()
+        self.hero.add_feat("Warrior", "Once per short rest, you can take a bonus action.")
+        self.hero.add_feat("Tamer", "Grants +2 Nature")
+        self.hero.add_feat("Alert", "+ Gain +5 Initiative\n"
+                               "+ Can't be surprised while conscious"
+                               "\n+ No stealth advantage for attackers.")
 
         # Window Details
         # ========================================================
@@ -24,23 +24,22 @@ class MainMenu:
         self.master.iconbitmap('C:\\Users\\elite\\Pictures\\Icons\\cog.ico')
         self.master.minsize(500, 300)
 
+        # Textbox(es)
+        # ========================================================
+        self.tb_feats = tk.Text(self.master, height=24, width=30, font=('Comic Sans MS', 8))
+        self.tb_feats.grid(row=0, column=0, rowspan=6, padx=10, pady=(10, 0), sticky=tk.N)
+        self.tb_feats.config(state=tk.DISABLED)
+
         # Listbox(es)
         # ========================================================
         self.featList = tk.Listbox(self.master, height=20, width=20)
         self.featList.grid(row=0, column=1, columnspan=3, pady=(10, 0), sticky=tk.N)
-
-        for i in range(hero.get_feat_size()):
-            self.featList.insert(tk.END, hero.get_feat(i))
-
-        # Textbox(es)
-        # ========================================================
-        tb_feats = tk.Text(self.master, height=24, width=30, font=('Comic Sans MS', 8))
-        tb_feats.grid(row=0, column=0, rowspan=6, padx=10, pady=(10, 0), sticky=tk.N)
-        tb_feats.config(state=tk.DISABLED)
+        self.update_feats()
+        self.featList.bind('<Double-Button-1>', lambda x: self.write_feat_desc())
 
         # Button(s)
         # ========================================================
-        bt_add = tk.Button( # our adding button
+        self.bt_add = tk.Button( # our adding button
                         master=self.master,
                         text="+",
                         font=('Comic Sans MS', 10),
@@ -48,10 +47,9 @@ class MainMenu:
                         fg="white",
                         width=3,
                         height=0,
-                        command=lambda:[
-                        ]
+                        command=lambda: self.add_feat()
                     )
-        bt_remove = tk.Button( # our removing button
+        self.bt_remove = tk.Button( # our removing button
                         master=self.master,
                         text="-",
                         font=('Comic Sans MS', 10),
@@ -62,56 +60,58 @@ class MainMenu:
                         command=lambda:[
                         ]
                     )
-        bt_view = tk.Button( # our viewing button
-                        master=self.master,
-                        text="View",
-                        font=('Comic Sans MS', 10),
-                        bg="black",
-                        fg="white",
-                        width=4,
-                        height=0,
-                        command=lambda:[
-                            tb_feats.config(state=tk.NORMAL),       # makes it editable.
-                            tb_feats.delete("1.0", tk.END),         # clears it.
-                            tb_feats.insert(tk.END,                 # gets the right description.
-                                            hero.get_feat(self.featList.get(self.featList.curselection()))),
-                            tb_feats.config(state=tk.DISABLED)      # makes it un-editable again.
-                        ]
-                    )
 
-        bt_view.grid(row=1, column=1, pady=0, sticky=tk.N)
-        bt_add.grid(row=1, column=2, pady=0, sticky=tk.N)
-        bt_remove.grid(row=1, column=3, pady=0, sticky=tk.N)
+        self.bt_add.grid(row=1, column=2, pady=0, sticky=tk.N)
+        self.bt_remove.grid(row=1, column=3, pady=0, sticky=tk.N)
 
+    def add_feat(self):
+        AddFeat(self.master, self.hero)
+        self.update_feats()
+
+    def update_feats(self):
+        # self.featList.delete(0, tk.END)
+        for i in range(self.hero.get_feat_size()):
+            self.featList.insert(tk.END, self.hero.get_feat(i))
+            print(self.hero.get_feat(i))
+        # self.featList.update()
+
+    def write_feat_desc(self):
+        self.tb_feats.config(state=tk.NORMAL),  # makes it editable.
+        self.tb_feats.delete("1.0", tk.END),  # clears it.
+        self.tb_feats.insert(tk.END,  # gets the right description.
+                             self.hero.get_feat(self.featList.get(self.featList.curselection()))),
+        self.tb_feats.config(state=tk.DISABLED)
 
 class AddFeat(MainMenu):
-    def __init__(self, master):
+    def __init__(self, master, hero):
         self.master = master
+        self.hero = hero
+        self.master = tk.Toplevel()
 
         # Window Details
         # ========================================================
-        self.master.title("Hero Viewer")  # Window title
-        self.master.geometry("1000x500")  # default window size
+        self.master.title("Add Feature or Trait")  # Window title
+        self.master.geometry("300x200")  # default window size
         self.master.configure(bg="#038387")  # background color
         self.master.iconbitmap('C:\\Users\\elite\\Pictures\\Icons\\cog.ico')
-        self.master.minsize(500, 300)
+        self.master.minsize(300, 200)
 
         # Label(s)
         # ========================================================
-        name = tk.Label(self.master, text="Name").grid(row=0)
-        desc = tk.Label(self.master, text="Details").grid(row=0)
+        self.name = tk.Label(self.master, text="Name", bg="#038387").grid(row=0)
+        self.desc = tk.Label(self.master, text="Details", bg="#038387").grid(row=1)
 
         # Entries
         # ========================================================
-        e_name = tk.Entry(self.master)
-        e_desc = tk.Entry(self.master)
+        self.e_name = tk.Entry(self.master)
+        self.e_desc = tk.Entry(self.master)
 
-        e_name.grid(row=0, column=1)
-        e_desc.grid(row=1, column=1)
+        self.e_name.grid(row=0, column=1)
+        self.e_desc.grid(row=1, column=1)
 
         # Buttons
         # ========================================================
-        bt_submit = tk.Button(  # for finalizing your inputs.
+        self.bt_submit = tk.Button(  # for finalizing your inputs.
             master=self.master,
             text="Add",
             font=('Comic Sans MS', 10),
@@ -119,10 +119,12 @@ class AddFeat(MainMenu):
             fg="white",
             width=3,
             height=0,
-            command=lambda: [
+            command=lambda:[
+                self.hero.add_feat(self.e_name.get(), self.e_desc.get()),
+                self.master.destroy()
             ]
         )
-        bt_cancel = tk.Button(  # for cancelling the request
+        self.bt_cancel = tk.Button(  # for cancelling the request
             master=self.master,
             text="Cancel",
             font=('Comic Sans MS', 10),
@@ -130,8 +132,10 @@ class AddFeat(MainMenu):
             fg="white",
             width=3,
             height=0,
-            command=quit
+            command=self.master.destroy
         )
+        self.bt_cancel.grid(row=2, column=1, pady=0, sticky=tk.N)
+        self.bt_submit.grid(row=2, column=2, pady=0, sticky=tk.N)
 
 
 root = tk.Tk()
