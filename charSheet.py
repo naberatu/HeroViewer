84ad4438647a442f6aa39a96aa9b18435a035794
naberatu <naberatu@gmail.com>
2020-12-08 13:35:37 -0800

    Fixed bugs with loading and saving. Put guard against wrong hero_data.

--- charSheet.py
+++ charSheet.py
@@ -5,11 +5,11 @@ import pickle
 
 class CharSheet:
     def __init__(self):
-        # try:
-        self.stats = pickle.load(open("hero_info.p", "rb"))
-        # except:
-        #     self.stats = StatBlock().get()
-        #     pickle.dump(self.stats, open("hero_info.p", "wb"))
+        try:
+            self.stats = pickle.load(open("hero_info.p", "rb"))
+        except:
+            self.stats = StatBlock().get()
+            pickle.dump(self.stats, open("hero_info.p", "wb"))
 
         self.modifier = {
             "strength": self.calc_modifier("strength"),
@@ -21,19 +21,7 @@ class CharSheet:
         }
         self.ds_success = 0
         self.ds_fail = 0
-
-        # self.personality = None
-        # self.ideals = None
-        # self.bonds = None
-        # self.flaws = None
-
-        # self.held_weapon = None
-        # self.worn_armor = None
-
         self.feats_traits = pickle.load(open("feats_dict.p", "rb"))
-        # self.prof_langs = {}
-        # self.inventory = []
-        # self.spells = {}
 
         # load_feats = {"Warrior": "Once per short rest, you can take a bonus action.",
         #               "Tamer": "Grants +2 Nature",
@@ -75,30 +63,6 @@ class CharSheet:
 
     # Accessors
     # =========================================================
-    # def get_name(self):
-    #     return self.name
-
-    # def get_id(self):
-    #     return self.id
-
-    # def get_role(self):
-    #     return self.role
-
-    # def get_level(self):
-    #     return self.level
-
-    # def get_race(self):
-    #     return self.race
-
-    # def get_background(self):
-    #     return self.background
-
-    # def get_alignment(self):
-    #     return self.alignment
-
-    # def get_base_stat(self, stat):
-    #     return self.stat_block.get_stat(stat)
-
     def get_modifier(self, modifier):
         try:
             return self.modifier[modifier]
@@ -114,24 +78,6 @@ class CharSheet:
     def get_ds_fail(self):
         return self.ds_fail
 
-    # def get_personality(self):
-    #     return self.personality
-    #
-    # def get_ideals(self):
-    #     return self.ideals
-    #
-    # def get_bonds(self):
-    #     return self.bonds
-    #
-    # def get_flaws(self):
-    #     return self.flaws
-    #
-    # def get_weapon(self):
-    #     return self.held_weapon
-    #
-    # def get_armor(self):
-    #     return self.worn_armor
-    #
     def get_feat_name(self, index):
         return list(self.feats_traits.keys())[index]
 
@@ -144,38 +90,8 @@ class CharSheet:
     def edit_feat(self, pair):
         self.feats_traits[pair[0]] = pair[1]
 
-    # def get_prof(self, prof):
-    #     for i in self.prof_langs:
-    #         if i == prof:
-    #             return self.prof_langs[i]
-    #         else:
-    #             return None
-
-    # def get_item(self, item):
-    #     for i in self.inventory:
-    #         if i == item:
-    #             return self.inventory[i]
-    #         else:
-    #             return None
-
-    # def get_spell(self, spell):
-    #     for i in self.spells:
-    #         if i == spell:
-    #             return self.spells[i]
-    #         else:
-    #             return None
-
     # Mutators
     # =========================================================
-    # def set_name(self, string):
-    #     self.name = string
-    #
-    # def set_id(self, num):
-    #     self.id = num
-    #
-    # def set_role(self, string):
-    #     self.role = string
-
     def level_up(self):
         if self.stats["level"] < 20:
             self.stats["level"] += 1
@@ -187,52 +103,12 @@ class CharSheet:
             return True
         return False
 
-    # def set_race(self, string):
-    #     self.race = string
-    #
-    # def set_background(self, string):
-    #     self.background = string
-    #
-    # def set_alignment(self, string):
-    #     self.alignment = string
-    #
-    # def set_base_stat(self, stat, num, face=None):
-    #     self.stat_block.modify_stat(stat, num, face)
-    #
-    # def set_modifier(self, modifier, value):
-    #     self.modifiers[modifier] = value
-    #
     def ds_success(self):
         self.ds_success += 1
 
     def ds_fail(self):
         self.ds_fail += 1
 
-    # def set_personality(self, string):
-    #     self.personality = string
-    #
-    # def set_ideals(self, string):
-    #     self.ideals = string
-    #
-    # def set_bonds(self, string):
-    #     self.bonds = string
-    #
-    # def set_flaws(self, string):
-    #     self.flaws = string
-    #
-    # def swap_weapon(self, swap):
-    #     temp = self.get_item(swap)
-    #     if temp is not None:
-    #         self.inventory.append(self.held_weapon)
-    #         self.held_weapon = temp
-    #
-    # def swap_armor(self, swap):
-    #     temp = self.get_item(swap)
-    #     if temp is not None:
-    #         self.inventory.append(self.worn_armor)
-    #         self.worn_armor = temp
-
-    # def add_feat(self, name, desc, name_only):
     def add_feat(self, name, desc, original):
         if name:
             if not desc and name != original:
@@ -245,15 +121,3 @@ class CharSheet:
                 self.feats_traits[name] = desc
         if not name and desc:
             self.feats_traits[original] = desc
-
-    # def add_prof(self, name, desc):
-    #     self.prof_langs[name] = desc
-    #
-    # def add_to_inv(self, item):
-    #     self.inventory.append(item)
-    #
-    # def add_spell(self, name, desc):
-    #     self.spells[name] = desc
-
-
-
